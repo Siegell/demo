e11ad3fa539fc06ffda43a6ffac740c57a909bf4
Pavel Zinkevich <pavel-zin2012@yandex.by>
2018-10-15 16:30:39 +0300

    add edit contracts

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -4,17 +4,15 @@ import com.example.demo.model.Contract;
 import com.example.demo.model.ContractsRepository;
 import com.example.demo.model.Stage;
 import com.example.demo.model.StagesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Controller
 public class IndexController {
@@ -38,12 +36,42 @@ public class IndexController {
         stage.setBeginDate(LocalDate.now());
         stage.setName("test");
         stagesRepository.save(stage);
-        Set<Stage> stages = new LinkedHashSet<Stage>();
+        Set<Stage> stages = new LinkedHashSet<>();
         stages.add(stage);
         contract.setStages(stages);
         contractsRepository.save(contract);
-
-
         return new ModelAndView("index");
+    }
+
+    @GetMapping("/{contractID}/edit")
+    public ModelAndView editform(@PathVariable long contractID){
+        Contract contract = contractsRepository.findById(contractID).get();
+        Map<String, Object> model = new HashMap<>();
+        model.put("contractor", (contract.getContractor() != null ? contract.getContractor() : ""));
+        model.put("contractDate", (contract.getContractDate() != null ? contract.getContractDate() : ""));
+        model.put("beginDate", (contract.getBeginDate() != null ? contract.getBeginDate() : ""));
+        model.put("endDate", (contract.getEndDate() != null ? contract.getEndDate() : ""));
+        model.put("totalCost", (contract.getTotalCost() != null ? contract.getTotalCost() : ""));
+        return new ModelAndView("edit", model);
+    }
+
+    @PostMapping("/{contractID}/edit")
+    public ModelAndView save(@PathVariable long contractID,  @RequestParam Map<String, String> map){
+        Contract contract = contractsRepository.findById(contractID).get();
+        contract.setContractor(map.getOrDefault("contractor", null));
+        String beginDateStr = map.getOrDefault("beginDate", null);
+        if (!Objects.equals(beginDateStr, ""))
+            contract.setBeginDate(LocalDate.parse(beginDateStr));
+        String endDateStr = map.getOrDefault("endDate", null);
+        if (!Objects.equals(endDateStr, ""))
+            contract.setEndDate(LocalDate.parse(endDateStr));
+        String contractDateStr = map.getOrDefault("contractDate", null);
+        if (!Objects.equals(contractDateStr, ""))
+            contract.setContractDate(LocalDate.parse(contractDateStr));
+        String totalCostStr = map.getOrDefault("totalCost", "0");
+        if (!Objects.equals(totalCostStr, ""))
+            contract.setTotalCost(Long.parseLong(totalCostStr));
+        contractsRepository.save(contract);
+        return new ModelAndView("redirect:/");
     }
 }
