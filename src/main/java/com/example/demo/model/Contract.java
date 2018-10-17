a0fb1efdbec930ca9570c2ab1e9f42ec627dccba
Pavel Zinkevich <pavel-zin2012@yandex.by>
2018-10-17 20:58:12 +0300

    fix id generation

--- src/main/java/com/example/demo/model/Contract.java
+++ src/main/java/com/example/demo/model/Contract.java
@@ -10,7 +10,7 @@ import java.util.Set;
 @Entity
 public class Contract {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String contractor;
--- src/main/java/com/example/demo/model/Stage.java
+++ src/main/java/com/example/demo/model/Stage.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class Stage {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
