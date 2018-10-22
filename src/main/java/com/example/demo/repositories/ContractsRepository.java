592ad2c5adcf2783fa5322ee38e5c9d511c0097b
Pavel Zinkevich <pavel-zin2012@yandex.by>
2018-10-22 13:40:16 +0300

    Change CRUD repository to JPA repository

--- src/main/java/com/example/demo/repositories/ContractsRepository.java
+++ src/main/java/com/example/demo/repositories/ContractsRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repositories;
 
 import com.example.demo.domain.Contract;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContractsRepository extends CrudRepository<Contract, Long>{
+public interface ContractsRepository extends JpaRepository<Contract, Long> {
 }
