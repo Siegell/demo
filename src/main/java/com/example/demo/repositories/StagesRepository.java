119884868dc35beb78f4be91439836b0d655b4b7
Pavel Zinkevich <pavel-zin2012@yandex.by>
2018-10-23 17:57:31 +0300

    add annotations

--- src/main/java/com/example/demo/repositories/StagesRepository.java
+++ src/main/java/com/example/demo/repositories/StagesRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.repositories;
 import com.example.demo.domain.Contract;
 import com.example.demo.domain.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
+@Repository
 public interface StagesRepository extends JpaRepository<Stage, Long> {
     Set<Stage> findStagesByContract(Contract contract);
 }
