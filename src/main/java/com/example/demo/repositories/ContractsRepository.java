61b2d6b7111c41c61fb9cdcd289c3df08517c3eb
Pavel Zinkevich <pavel-zin2012@yandex.by>
2018-10-25 14:16:53 +0300

    add contractors repository

--- src/main/java/com/example/demo/repositories/ContractsRepository.java
+++ src/main/java/com/example/demo/repositories/ContractsRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repositories;
 
 import com.example.demo.domain.Contract;
+import com.example.demo.domain.Contractor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ContractsRepository extends JpaRepository<Contract, Long> {
+    Set<Contract> findContractByContractor(Contractor contractor);
 }
